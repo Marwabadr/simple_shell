37d5ba0e7bbabd7f076ee607f7cee86a5ae9ae47
Marwabadr <elbadrmarwa2002@gmail.com>
2023-11-16 23:27:31 +0100

    simple shell

--- simpleshell.h
+++ simpleshell.h
@@ -91,7 +91,7 @@ typedef struct passInf
 	int histcount;
 } infoP;
 
-#define INFOINIT \
+#define INFO_INIT \
 {NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
 	0, 0, 0}
 
